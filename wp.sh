0bf5e6cf16ba2819d0a5c808e49733b767cdae05
Dawid Rybacki <dawidryba@drybacki.talem.eu>
2018-11-13 13:56:57 +0100

    Add env support

--- wp.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-wp plugin install w3-total-cache
-wp plugin install duplicate-post
-wp plugin install loco-translate
-wp plugin install contact-form-7
-wp plugin install all-in-one-seo-pack
